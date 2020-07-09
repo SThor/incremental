fb30f1a182dfd4f6980769ac2a826b95291fd340
Paul Givel <paulgivel@gmail.com>
2020-07-09 15:51:38 +0200

    give default values for combat if contract didn't provide any

--- src/BattleComponent.js
+++ src/BattleComponent.js
@@ -10,10 +10,12 @@ export class BattleComponent extends Component {
   }
 
   render() {
+    let health = this.props.health?this.props.health:10;
+    let time = this.props.time?this.props.time:30;
     return (
       <div className={styles.battleComponent}>
-        <HoverComponent style={{"flex-grow":1}} text={this.props.text} target={this.props.health} onFinished={this.props.onSuccess}/>
-        <TimerComponent onFinished={this.props.onFail} target={this.props.time}/>
+        <HoverComponent style={{"flex-grow":1}} text={this.props.text} target={health} onFinished={this.props.onSuccess}/>
+        <TimerComponent onFinished={this.props.onFail} target={time}/>
       </div>
     );
   }
