5c29d2457cfcef3f1a1fc91364f889b698ad3262
Paul Givel <paulgivel@gmail.com>
2020-08-20 23:03:57 +0200

    extracted Prologue to StoryMode component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, { Component } from "react";
 import "./App.css";
-import ProgressButton from "./ProgressButton";
 import TabView from "./Tabs/TabView";
 import TabContent from "./Tabs/TabContent";
 import BattleComponent from "./BattleComponent";
 import ContractsTab from "./Contracts/ContractsTab";
 import Modal from "./Modal";
+import StoryMode from "./StoryMode";
 
 export class App extends Component {
   constructor() {
     super();
     this.state = this.recoverState();
 
-
-    this.onMainButton = this.onMainButton.bind(this);
-    this.onFail = this.onFail.bind(this);
-    this.skipPrologue = this.skipPrologue.bind(this);
     this.getDefaultState = this.getDefaultState.bind(this);
     this.resetState = this.resetState.bind(this);
     this.storeState = this.storeState.bind(this);
     this.recoverState = this.recoverState.bind(this);
+    this.endStory = this.endStory.bind(this);
+    this.setChapter = this.setChapter.bind(this);
   }
 
   getDefaultState() {
     return Object.assign({}, {
-      currentStade: 0,
+      story: {chapter:0},
       currentTitle: this.stades[0].title,
-      currentButtonText: this.stades[0].buttonText[0],
-      currentInfoText: this.stades[0].infoText[0],
-      currentSubStade: 0,
-      combatInProgress: true,
     });
   }
 
@@ -40,78 +34,21 @@ export class App extends Component {
 
   stades = [
     {
-      title: "Prologue",
-      buttonText: [
-        "Wake up",
-        "Pick up the sword",
-        "Swing your sword at it",
-        "Take refuge in the cave",
-        "Drift into a deep slumber",
-        "Read the note",
-      ],
-      infoText: [
-        "",
-        "You see a unusually long and heavy sword lying next to you.",
-        "As you pick it up, you notice from the corner of your eye a wounded boar charging at you.",
-        "After defeating the boar, you spot a hidden cave nearby that feels safe.",
-        "You found refuge in the cave.",
-        "On waking up your body is still aching all over. You find a note thanking you for defeating the boar, along with a nice round coin.",
-      ],
-      initStade: () => {
-        this.stades[0].setSubStade(0);
-      },
-      setSubStade: (index) => {
-        if (index >= this.stades[this.state.currentStade].buttonText.length) {
-          this.setStade(1);
-        } else {
-          console.log("setting substade to", index);
-          this.setState({
-            currentSubStade: index,
-            currentButtonText: this.stades[this.state.currentStade].buttonText[
-              index
-            ],
-            currentInfoText: this.stades[this.state.currentStade].infoText[
-              index
-            ],
-          });
-        }
-      },
+      title: "Prologue"
     },
     {
       title: "Beginning of an empire",
-      initStade: () => {},
       contracts: [{ title: "thanks for the boar", contents: "thanks." },{ title: "thanks for the boar", contents: "thanks." }],
     },
   ];
 
-  setStade(index) {
-    index = Math.min(index, this.stades.length - 1);
-    console.log("setting stade to", index);
+  endStory() {
+    this.setState({story:undefined});
+  }
+  setChapter(){
     this.setState({
-      currentStade: index,
-      currentTitle: this.stades[index].title,
+      story: {chapter:this.state.story.chapter+1},
     });
-    this.stades[index].initStade();
-  }
-
-  onMainButton() {
-    if (this.state.currentStade === 0) {
-      this.stades[0].setSubStade(this.state.currentSubStade + 1);
-    } else {
-      this.setStade(this.state.currentStade + 1);
-    }
-  }
-
-  onFail() {
-    this.setStade(0);
-    this.setState({
-      currentInfoText:
-        "The last thing you saw was the boar's tusk ramming in your leg before you fell uncounscious.",
-    });
-  }
-
-  skipPrologue() {
-    this.setStade(1);
   }
 
   resetState() {
@@ -132,38 +69,18 @@ export class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.currentStade !== 0 || this.state.currentSubStade !== 0 ? (
-          <h1>{this.state.currentTitle}</h1>
-        ) :
-        (<button onClick={this.skipPrologue} style={{ "fontSize": "x-small" }}>
-          Skip prologue
-        </button>)}
-        {this.state.currentStade === 0 ? (
-          <TabContent>
-            <p>{this.state.currentInfoText}</p>
-            {this.state.currentStade === 0 &&
-            this.state.currentSubStade === 2 ? (
-              <BattleComponent
-                text={this.state.currentButtonText}
-                onSuccess={this.onMainButton}
-                onFail={this.onFail}
-                health={10}
-                time={30}
-              />
-            ) : (
-              <ProgressButton
-                text={this.state.currentButtonText}
-                onFinished={this.onMainButton}
-              />
-            )}
-          </TabContent>
+        {typeof this.state.story != "undefined" ? (
+          <StoryMode onEndStory={this.endStory} story={this.state.story} onSetChapter={this.setChapter}></StoryMode>
         ) : (
-          <TabView showControls={this.state.currentStade > 0}>
+          <div>
+          <h1>{this.state.currentTitle}</h1>
+          <TabView showControls={true}>
             <ContractsTab
-              contracts={this.stades[this.state.currentStade].contracts}
+              contracts={this.stades[1].contracts}
             ></ContractsTab>
             <TabContent>Your den</TabContent>
           </TabView>
+          </div>
         )}
       <button onClick={this.resetState} style={{ "fontSize": "x-small", "position": "absolute", "bottom": "0" }}>Reset state</button>
         <Modal active={false}><BattleComponent></BattleComponent></Modal>
