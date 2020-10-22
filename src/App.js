323c036f033160ae5ef2dcc5ef2ff9e81099859c
Faugère Gaëtan <gaetan@fauge.re>
2020-10-22 23:58:37 +0200

    Extracted game content from game mechanics
    Co-authored-by: Paul Givel <69793750+pgivel@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ContractsTab from "./Contracts/ContractsTab";
 import Modal from "./Modal";
 import StoryMode from "./StoryMode";
 
+import chapters from "./gameContent.js";
+
 export class App extends Component {
   constructor() {
     super();
@@ -31,50 +33,6 @@ export class App extends Component {
     window.addEventListener('beforeunload', this.storeState);
     setInterval(this.storeState, 60000);
   }
-
-  chapters = [
-    {
-      type: "story",
-      title: "Prologue",
-      failText: "The last thing you saw was the boar's tusk ramming in your leg before you fell uncounscious.",
-      paragraphs: [
-        {
-          info: "",
-          button: "Wake up",
-          hideTitle: true
-        },
-        {
-          info: "You see a unusually long and heavy sword lying next to you.",
-          button: "Pick up the sword"
-        },
-        {
-          info: "As you pick it up, you notice from the corner of your eye a wounded boar charging at you.",
-          button: "Swing your sword at it",
-          battle: {
-            health: 10,
-            time: 30
-          }
-        },
-        {
-          info: "After defeating the boar, you spot a hidden cave nearby that feels safe.",
-          button: "Take refuge in the cave"
-        },
-        {
-          info: "You found refuge in the cave.",
-          button: "Drift into a deep slumber"
-        },
-        {
-          info: "On waking up your body is still aching all over. You find a note thanking you for defeating the boar, along with a nice round coin.",
-          button: "Read the note"
-        }
-      ]
-    },
-    {
-      type: "adventure",
-      title: "Beginning of an empire",
-      contracts: [{ title: "thanks for the boar", contents: "thanks." },{ title: "thanks for the boar", contents: "thanks." }],
-    }
-  ];
 
   setParagraph(paragraph = (this.state.currentParagraph + 1)) {
     this.setState({
@@ -104,7 +62,7 @@ export class App extends Component {
   }
 
   render() {
-    let chapter = this.chapters[this.state.currentChapter];
+    let chapter = chapters[this.state.currentChapter];
     return (
       <div className="App">
         { chapter.type === "story" ? (
